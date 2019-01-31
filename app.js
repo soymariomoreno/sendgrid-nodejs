660c1373ce22cc4d8dac96f0bcb5d2ccc5d71fb1
Mario Moreno <mario.moreno.a93@gmail.com>
2019-01-30 20:51:37 -0600

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+sgMail.setApiKey('KEY');
+
 const msg = {
   to: 'test@example.com',
   from: 'test@example.com',
@@ -9,4 +11,5 @@ const msg = {
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
+
 sgMail.send(msg);
---
+++ readme.md
@@ -0,0 +1,7 @@
+Pasos para iniciar
+
+1.- Descargar el proyecto o clonarlo
+2.- Dentro del proyecto hacer un npm install
+3.- Después de eso modificar el archivo agregando el key de sendgrid como usuarios
+4.- Agregar correo emisor y receptor
+5.- Correr el programa con node app.js dentro de la misma terminal de la carpeta 
